bfa8614a01d36e3cb2f11298e64136490f7916e8
Creador0 <79320055+Creador0@users.noreply.github.com>
2021-02-26 09:09:38 -0600

    Finalización

--- src/com/softgame/poo1game/pruebas/PruebaPersonaje.java
+++ src/com/softgame/poo1game/pruebas/PruebaPersonaje.java
@@ -10,24 +10,25 @@ public class PruebaPersonaje{
 		per03.setEdad(1);
 		Personaje per04 = new Personaje("Lesly");
 		per04.setEdad(-10);
+		outfor:
 		for(int i = 0;i < 3;i++){
 			int ed = (int)(Math.random()*251);
 			if(per01.setEdad(ed)){
-				System.out.println(per01.getDetalle()+" Se modificó la edad");
+				System.out.println(per01.getDetalle()+" Se modifico la edad");
 			}else{
-				System.out.println(per01.getDetalle() + " sin modificación en edad a " + ed);
+				System.out.println(per01.getDetalle() + " sin modificacion en edad a " + ed);
 			}
-			int ed = (int)(Math.random()*251);
+			ed = (int)(Math.random()*251);
 			if(per02.setEdad(ed)){
-				System.out.println(per02.getDetalle()+" Se modificó la edad");
+				System.out.println(per02.getDetalle()+" Se modifico la edad");
 			}else{
-				System.out.println(per02.getDetalle() + " sin modificación en edad a " + ed);
+				System.out.println(per02.getDetalle() + " sin modificacion en edad a " + ed);
 			}
-			int ed = (int)(Math.random()*251);
+			ed = (int)(Math.random()*251);
 			if(per01.setEdad(ed)){
-				System.out.println(per03.getDetalle()+" Se modificó la edad");
+				System.out.println(per03.getDetalle()+" Se modifico la edad");
 			}else{
-				System.out.println(per03.getDetalle() + " sin modificación en edad a " + ed);
+				System.out.println(per03.getDetalle() + " sin modificacion en edad a " + ed);
 			}
 			do{
 				ed = (int)(Math.random()*251);
