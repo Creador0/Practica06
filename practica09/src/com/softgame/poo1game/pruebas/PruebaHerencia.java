ac8778ba3d6e8312bb60878c480bf6207c710df2
Ricardo Sánchez <creador11001@gmial.com>
2021-03-12 09:19:52 -0600

    Subida 8
    Se corrigieron errores concerniente a los ciclos anidad, específicamente con respecto a "i"

--- practica09/src/com/softgame/poo1game/pruebas/PruebaHerencia.java
+++ practica09/src/com/softgame/poo1game/pruebas/PruebaHerencia.java
@@ -5,23 +5,24 @@ import com.softgame.poo1game.personajes.malos.Zombie;
 
 public class PruebaHerencia{
 	public static void main(String args[]){
-		Personaje[] personajes = new Personaje("Omar",99), new Planta("Karen",50),
-		new Zombie("Hanna",80,false), new personajes("Omar",99), new Planta("Karen",50),
-		new Zombie("Hanna",80,false);
+		Personaje[] personajes = {new Personaje("Omar",99), new Planta("Karen",50),
+		new Zombie("Hanna",80,false), new Personaje("Omar",99), new Planta("Karen",50),
+		new Zombie("Hanna",80,false)};
 		
-		while(i < personajes.length()){
+		int i = 0;
+		while(i < personajes.length){
 			for(Personaje p: personajes){
-				if(i == p){
-					System.out.println("***" + i.toString() + "es identico a" p.toString()
-					+ "Son el mismo objeto");
+				if(personajes[i] == p){
+					System.out.println("***" + personajes[i].toString() + " es identico a " + p.toString() + " Son el mismo objeto");
 				}
-				if(i.equals(p)){
-					System.out.println("**" + i.toString() + "es igual a" + p.toString()
-					+ "Son diferentes objetos");
+				if(personajes[i].equals(p)){
+					System.out.println("**" + personajes[i].toString() + " es igual a " + p.toString()
+					+ " Son diferentes objetos");
 				}else{
-					System.out.println(i.toString() + "!=" + p.toString());
+					System.out.println(personajes[i].toString() + " != " + p.toString());
 				}
 			}
+			i++;
 		}
 	}
 }
