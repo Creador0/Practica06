19c85890737a5ca81a83dd34f6f02ebaaa7f0ed3
Ricardo Sánchez <creador11001@gmial.com>
2021-03-12 08:02:54 -0600

    Create Personaje.java

---
+++ practica09/src/com/softgame/poo1game/personajes/Personaje.java
@@ -0,0 +1,56 @@
+package com.softgame.poo1game.personajes;
+public class Personaje{
+	protected String nombre;
+	protected int vida;
+	
+	public Personaje(String nombre){
+		this.vida = 3;
+		this.nombre = nombre;
+	}
+	
+	public Personaje(String nombre, int vida){
+		this.vida = vida;
+		this.nombre = nombre;
+	}
+	
+	public void setNombre(String nombre){
+		if(nombre.length() > 3 && nombre.length() < 10){
+			this.nombre = nombre;
+		}
+	}
+
+	public String getNombre(){
+		return nombre;
+	}
+
+	public void saludar(){
+		System.out.println("Hola alumno de POO " + nombre);
+	}
+		
+	public int getVida(){
+		return vida;
+	}	
+	public boolean setVida(int vida){
+		if(vida > 0 && vida < 100){
+			this.vida = vida;
+			return true;
+		} else {
+			return false;
+		}
+	}
+	public String toString(){
+		return (nombre + "\t" + vida);
+	}
+	
+	public void decVida(){
+		if(vida != 0){
+			vida = vida - 1;
+		}
+	}
+	
+	public void decVida(int dec){
+		if((vida - dec)>0){
+			vida = vida - dec;
+		}
+	}
+}
