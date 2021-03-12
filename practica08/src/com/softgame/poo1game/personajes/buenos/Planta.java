c9fb84e033ca478d3d23cebb9d1389c13154f49e
Ricardo Sánchez <creador11001@gmial.com>
2021-03-12 03:25:00 -0600

    Create Planta.java

---
+++ practica08/src/com/softgame/poo1game/personajes/buenos/Planta.java
@@ -0,0 +1,45 @@
+package com.softgame.poo1game.personajes.buenos;
+import com.softgame.poo1game.personajes.Personaje;
+public class Planta extends Personaje{
+	char escudo;
+	public Planta(String nombre, int vida, char escudo){
+		super(nombre,vida);
+		this.escudo = escudo;
+	}
+	
+	public Planta(String nombre, char escudo){
+		this(nombre,3,escudo);
+	}
+	
+	public Planta(String nombre, int vida){
+		this(nombre,vida,'A');
+	}
+	
+	public Planta(String nombre){
+		this(nombre,3,'A');
+	}
+	
+	public char getEscudo(){
+		return escudo;
+	}
+	
+	public String getDetalle(){
+		return super.getDetalle()+" "+escudo;
+	}
+	
+	public void decVida(){
+		if(escudo == 'A'){
+			super.decVida(2);
+		}else{
+			super.decVida();
+		}
+	}
+	
+	public void decVida(int dec){
+		if(escudo == 'A'){
+			super.decVida(dec * 2);
+		}else{
+			super.decVida(dec);
+		}
+	}
+}
