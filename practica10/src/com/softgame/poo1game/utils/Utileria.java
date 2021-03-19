7ed33f433c932b2959007d88e33261d24c2b9960
Ricardo Sánchez <creador11001@gmial.com>
2021-03-19 09:13:50 -0600

    Cambio 5
    Se agrega el paquete en Utileria y contadorID se vuelve static - 09:13

--- practica10/src/com/softgame/poo1game/utils/Utileria.java
+++ practica10/src/com/softgame/poo1game/utils/Utileria.java
@@ -1,10 +1,11 @@
+package com.softgame.poo1game.utils;
 public class Utileria{
-	public int contadorID;
+	private static int contadorID = 1;
 	private Utileria(){
 		
 	}
 	public static int getID(){
-		return contadorID + 1
+		return contadorID = contadorID + 1;
 	}
 	
 }
