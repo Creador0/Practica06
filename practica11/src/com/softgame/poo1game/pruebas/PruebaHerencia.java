10d12598d9b6e4763f4201be2772f57eb8d6fd61
Ricardo Sánchez <creador11001@gmial.com>
2021-03-26 09:15:53 -0600

    Create PruebaHerencia.java

---
+++ practica11/src/com/softgame/poo1game/pruebas/PruebaHerencia.java
@@ -0,0 +1,31 @@
+package com.softgame.poo1game.pruebas;
+import com.softgame.poo1game.personajes.Personaje;
+import com.softgame.poo1game.personajes.buenos.Planta;
+import com.softgame.poo1game.personajes.malos.Zombie;
+
+public class PruebaHerencia{
+	public static void main(String args[]){
+		Personaje[] personajes = {new Personaje("Omar",99), new Planta("Karen",50),
+		new Zombie("Hanna",80,false), new Personaje("Omar",99), new Planta("Karen",50),
+		new Zombie("Hanna",80,false), new Planta("Hanna",80,'A')};
+		System.out.println(personajes[2].getIdVida());
+		int i = 0;
+		while(i < personajes.length){
+			for(Personaje p: personajes){
+				if(personajes[i] == p){
+					System.out.println("***" + personajes[i].toString() + " es identico a " + p.toString() + " Son el mismo objeto***");
+					continue;
+				}
+				if(personajes[i].equals(p)){
+					System.out.println("**" + personajes[i].toString() + " es igual a " + p.toString()
+					+ " Son diferentes objetos ***");
+					continue;
+				}else{
+					System.out.println(personajes[i].toString() + " != " + p.toString());
+				}
+			}
+			i++;
+			System.out.println();
+		}
+	}
+}
