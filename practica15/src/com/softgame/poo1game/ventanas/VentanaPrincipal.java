4fe1b1ac1d1449b8f80c975975f3f7d0e402cb76
Ricardo Sánchez <creador11001@gmial.com>
2021-05-07 09:40:36 -0500

    Update VentanaPrincipal.java

--- practica15/src/com/softgame/poo1game/ventanas/VentanaPrincipal.java
+++ practica15/src/com/softgame/poo1game/ventanas/VentanaPrincipal.java
@@ -1,1 +1,11 @@
-public class VentanaPrincipal
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class VentanaPrincipal extends JFrame implements ActionListener{
+	private JFrame f:
+	private JLabel lblFile, lblN, lblLeidos;
+	private JTextField txtFile;
+	private JButton btnOpen, btnExit;
+	private JTextArea txtContenido;
+	
+}
