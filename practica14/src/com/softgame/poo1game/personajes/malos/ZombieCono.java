4bce9b2b9dfee5761b03fb1d7aebdc63db7fda07
Ricardo Sánchez <creador11001@gmial.com>
2021-05-07 09:20:38 -0500

    Create ZombieCono.java

---
+++ practica14/src/com/softgame/poo1game/personajes/malos/ZombieCono.java
@@ -0,0 +1,8 @@
+package com.softgame.poo1game.personajes.malos;
+import com.softgame.poo1game.personajes.Personaje;
+import com.softgame.poo1game.personajes.malos.Zombie;
+public class ZombieCono extends Zombie{
+	public ZombieCono(String nombre){
+		super(nombre,3,false);
+	}
+}
