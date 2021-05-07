2c1d9602b7330b7f92938d78c63fdaccc6b5bec0
Ricardo Sánchez <creador11001@gmial.com>
2021-05-07 09:23:10 -0500

    Create TheException.java

---
+++ practica14/src/com/softgame/poo1game/utils/TheException.java
@@ -0,0 +1,12 @@
+package com.softgame.poo1game.utils;
+public class TheException extends Exception{
+	private int poss;
+	public TheException(String msg, int poss){
+		super(msg);
+		this.poss= poss;
+	}
+
+	public int getPoss(){
+		return poss;
+	}
+}
