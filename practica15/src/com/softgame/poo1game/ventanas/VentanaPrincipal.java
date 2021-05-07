7a7957b5c0dc089a99b39931f8cc1b0313d292c7
Ricardo Sánchez <creador11001@gmial.com>
2021-05-07 09:48:12 -0500

    Update VentanaPrincipal.java

--- practica15/src/com/softgame/poo1game/ventanas/VentanaPrincipal.java
+++ practica15/src/com/softgame/poo1game/ventanas/VentanaPrincipal.java
@@ -8,4 +8,15 @@ public class VentanaPrincipal extends JFrame implements ActionListener{
 	private JButton btnOpen, btnExit;
 	private JTextArea txtContenido;
 	
+	public VentanaPrincipal(){
+		f = new JFrame("Practica 14");
+		lblFile = new JLabel("Nombre de archivo");
+		txtFile = new JTextField("");
+		btnOpen = new JButton("Abrir archivo");
+		txtContenido = new JTextArea();
+		lblLeidos = new JLabel("Caracteres leidos");
+		lblN = new JLabel("0");
+		btnExit = new JButton("Salir");
+		
+	}
 }
