8e0c8f317bdd08bbf977d48097620adc5c8959ab
Ricardo Sánchez <creador11001@gmial.com>
2021-05-14 09:12:48 -0500

    Update VentanaHilo.java

--- practica16/src/com/softgame/poo1game/ventanas/VentanaHilo.java
+++ practica16/src/com/softgame/poo1game/ventanas/VentanaHilo.java
@@ -4,9 +4,25 @@ import javax.swing.JMenu;
 import javax.swing.JMenuItem;
 import javax.swing.JFrame;
 public class VentanaHilo extends JFrame{
+	private JFrame f;
 	private JMenuBar menuBar;
 	private JLabel lblA;
 	private JLabel lblB;
 	private JLabel lblC;
 	private JLabel lblD;
+	private JMenu mnuArchivo;
+	private JMenuItem mnuInicio;
+	private JMenuItem mnuSalir;
+	
+	public void initComponents(){
+		f.setLayout(new GridLayout(4,1));
+		menuBar = new JMenuBar();
+		f.setJMenuBar(menuBar);
+		mnuArchivo = new JMenu("Archivo");
+		menuBar.add(mnuArchivo);
+		mnuInicio = new JMenuItem("Inicio");
+		mnuArchivo.add(mnuInicio);
+		mnuSalir = new JMenuItem("Salir");
+		mnuArchivo.add(mnuSalir);
+	}
 }
