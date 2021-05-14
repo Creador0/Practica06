2efc389fa90eb1ef48b6d8a38e2255c445c5fcb6
Ricardo Sánchez <creador11001@gmial.com>
2021-05-14 09:55:46 -0500

    Create PruebaHilos.java

---
+++ practica16/src/com/softgame/poo1game/pruebas/PruebaHilos.java
@@ -0,0 +1,8 @@
+package com.softgame.poo1game.pruebas;
+import com.softgame.poo1game.ventanas.VentanaHilo;
+public class PruebaHilos{
+	public static void main(String args[]){
+		VentanaHilo v = new VentanaHilo();
+		v.setVisible(true);
+	}
+}
