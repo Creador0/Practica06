a515193b735698625830ee3583e905a06a67ae05
Ricardo Sánchez <creador11001@gmial.com>
2021-05-14 08:52:12 -0500

    Create VentanaHilo.java

---
+++ practica16/src/com/softgame/poo1game/ventanas/VentanaHilo.java
@@ -0,0 +1,12 @@
+import javax.swing.JMenuBar;
+import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
+import javax.swing.JFrame;
+public class VentanaHilo extends JFrame{
+	private JMenuBar menuBar;
+	private JLabel lblA;
+	private JLabel lblB;
+	private JLabel lblC;
+	private JLabel lblD;
+}
