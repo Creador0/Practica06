86c85bd4c4fd5cc774040d28f67486063f5e17a9
Ricardo Sánchez <creador11001@gmial.com>
2021-06-04 10:53:18 -0500

    Create Inicio.java

---
+++ ordinario12021/src/mx/edu/labpoo/ordinario/Inicio.java
@@ -0,0 +1,8 @@
+package mx.edu.labpoo.ordinario;
+import mx.edu.labpoo.ordinario.vistas.Ventana;
+public class Inicio {
+    public static void main(String[] args) {
+        Ventana v = new Ventana();
+        v.setVisible(true);
+    }
+}
